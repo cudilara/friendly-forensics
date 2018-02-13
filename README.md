6838455b7caeff1296aa78ceb36df7985342e010
cudilara <dilara.madinger@colorado.edu>
2018-02-13 14:31:33 -0700

    uploading file through webpage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # friendly-forensics
-This is Dilara Madinger's thesis project to automate aspects of forensic analysis of Raspberry Pie devices.
+This is Dilara Madinger's thesis project to automate aspects of forensic analysis of Raspberry Pi devices.
+
+### To run: export FLASK_APP=backServer.py; source servenv/bin/activate
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,10 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Volumes/XTRADISK/friendly-forensics/server/uploaded'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'file': 'file1',
@@ -15,4 +23,15 @@ def index():
             'body': 'test data for file 2'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file1():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
